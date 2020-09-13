be846187983c60f82decedaf961e45c7a426dbe6
MandeepM <maharjanmemandeep@gmail.com>
2020-09-14 00:18:32 +0545

    navbar  and initial  routing

---
+++ src/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./Header.css";
+import {Link} from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt=""></img> 
+            </Link>  
+
+            <div className="header_search">
+                <input type="text" className="header_searchInput" />  
+                <SearchIcon className="header_searchIcon" />  
+            </div>
+
+            <div className="header_nav">
+                {/* link 1 */}
+                <Link to="/login" className="header_link">
+                    <div className="header_option">
+                        <span className="header_option1">Hello</span>
+                        <span className="header_option2">Sign In</span>
+                    </div>
+                </Link>
+                {/* link 2 */}
+                <Link className="header_link">
+                    <div className="header_option">
+                        <span className="header_option1">Returns</span>
+                        <span className="header_option2">& Orders</span>
+                    </div>
+                </Link>
+                {/* link 3 */}
+                <Link className="header_link">
+                    <div className="header_option">
+                        <span className="header_option1">Your</span>
+                        <span className="header_option2">Prime</span>
+                    </div>
+                </Link>
+                {/* link 4 / basket */}
+                <Link to="/checkout" className="header_link">
+                    <div className="header_optionBasket">
+                         <ShoppingBasketIcon />
+                         <span className="header_option2 header_basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="home">
+      <img src="https://images-eu.ssl-images-amazon.com/images/G/02/digital" alt="" className="home_image"/> 
+    </div>
+  )
+}
+
+export default Home
